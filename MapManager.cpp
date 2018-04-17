fe32f03ae3ccabcf0cf3f47c815e96b617dd7bb2
Alban Nanty <banban@banban.org>
2018-04-17 22:28:54 +0800

    + draw an hard coded map

--- MapManager.cpp
+++ MapManager.cpp
@@ -5,9 +5,27 @@
 #include "MapManager.h"
 #include "SpriteData.h"
 
+#include <avr/pgmspace.h>
+
+const unsigned char level[][16] PROGMEM = {
+	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
+	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
+	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
+	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
+	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
+	{ SpriteData::STATUE_TOP, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
+	{ SpriteData::STATUE_BOTTOM, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
+	{ SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, },
+};
+
 void MapManager::Update()
 {
-	for (int i = 0; i < 10; ++i)
-		arduboy.drawBitmap(8*i, 10, SpriteData::Walls[0], 8, 8, WHITE);
+	for (int y = 0; y < 8; ++y)
+		for (int x = 0; x < 16; ++x)
+		{
+			unsigned char spriteId = pgm_read_byte(&(level[y][x]));
+			if (spriteId != SpriteData::NOTHING)
+				arduboy.drawBitmap(8*x, 8*y, SpriteData::Walls[spriteId], 8, 8, WHITE);
+		}
 	
 }
--- SpriteData.h
+++ SpriteData.h
@@ -3,6 +3,16 @@
 
 namespace SpriteData
 {
+	enum WallId
+	{
+		BLOCK_8_8 = 0,
+		BLOCK_16_8_LEFT,
+		BLOCK_16_8_RIGHT,
+		STATUE_BOTTOM,
+		STATUE_TOP,
+		NOTHING = 255,
+	};
+	
 	extern const unsigned char Walls[][8];
 }
 
