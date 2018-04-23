7b79cf1402a1911ce52079c360a1fdaf7df899ba
Alban Nanty <banban@banban.org>
2018-04-23 21:26:51 +0800

    + add an animation for transitioning the camera between two screens.

--- MapManager.cpp
+++ MapManager.cpp
@@ -16,7 +16,17 @@ const unsigned char level[][16] PROGMEM = {
 	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
 	{ SpriteData::STATUE_TOP, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
 	{ SpriteData::STATUE_BOTTOM, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
-	{ SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, },
+	{ SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, 255, 255, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, },
+	{ 0, 255, 255, 255, 255, 0, 255, 255, 0, 255, 255, 255, 255, 255, 255, 0, },
+	{ 0, 255, 255, 255, 255, 0, 255, 255, 0, 255, 255, 255, 255, 255, 255, 0, },
+	{ 0, 255, 255, 255, 255, 0, 255, 255, 0, 255, 255, 255, 255, 255, 255, 0, },
+	{ 0, 255, 255, 255, 255, 0, 255, 255, 0, 255, 255, 255, 255, 255, 255, 0, },
+	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, },
+	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, },
+	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, },
+	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, },
+	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, },
+	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, },
 };
 
 namespace MapManager
@@ -30,12 +40,79 @@ namespace MapManager
 	const int LEVEL_SPRITE_WIDTH = 8;
 	const int LEVEL_SPRITE_HEIGHT = 8;
 	
-	// The camera coordinate reference the top left corner of the screen portion of the level, in the big level array.
+	// The current camera coordinate reference the top left corner of the screen portion of the level, in the big level array.
 	int CameraX = 0;
 	int CameraY = 0;
+	
+	// The target camera coordinates
+	int TargetCameraX = 0;
+	int TargetCameraY = 0;
+	
+	// This variable is used to store a temporary shift of the camera during a transition animation
+	char CameraTransition = 0;
+	
+	void AnimateCameraTransition();
+	void Draw();
 }
 
 void MapManager::Update()
+{
+	// debug code
+	if (arduboy.pressed(DOWN_BUTTON))
+		TargetCameraY = 8;
+	if (arduboy.pressed(UP_BUTTON))
+		TargetCameraY = 0;
+	
+	AnimateCameraTransition();
+	Draw();
+}
+
+// this function check if there's difference between the current position of the camera
+// and the target position, and animate the camera position to reach the target
+void MapManager::AnimateCameraTransition()
+{
+	int xDiff = TargetCameraX - CameraX;
+	int yDiff = TargetCameraY - CameraY;
+	if (xDiff > 0)
+	{
+		CameraTransition++;
+		if (CameraTransition == LEVEL_SPRITE_WIDTH)
+		{
+			CameraTransition = 0;
+			CameraX++;
+		}
+	}
+	else if (xDiff < 0)
+	{
+		CameraTransition--;
+		if (CameraTransition == -LEVEL_SPRITE_WIDTH)
+		{
+			CameraTransition = 0;
+			CameraX--;
+		}
+	}
+	else if (yDiff > 0)
+	{
+		CameraTransition++;
+		if (CameraTransition == LEVEL_SPRITE_HEIGHT)
+		{
+			CameraTransition = 0;
+			CameraY++;
+		}
+	}
+	else if (yDiff < 0)
+	{
+		CameraTransition--;
+		if (CameraTransition == -LEVEL_SPRITE_HEIGHT)
+		{
+			CameraTransition = 0;
+			CameraY--;
+		}
+	}
+}
+
+// This function draw the map based on the current position of the camera.
+void MapManager::Draw()
 {
 	for (int y = 0; y < NB_VERTICAL_SPRITE_PER_SCREEN; ++y)
 		for (int x = 0; x < NB_HORIZONTAL_SPRITE_PER_SCREEN; ++x)
@@ -47,5 +124,4 @@ void MapManager::Update()
 					SpriteData::Walls[spriteId],
 					LEVEL_SPRITE_WIDTH, LEVEL_SPRITE_HEIGHT, WHITE);
 		}
-	
 }
