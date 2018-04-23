df8d29c351ce7d932b2f689701fb747983f6af88
Alban (Banban) Nanty <Alban (Banban) Nanty@localhost>
2018-04-23 20:54:19 +0800

    + add coordinates for the camera position + clean the code by adding some const values

--- MapManager.cpp
+++ MapManager.cpp
@@ -13,19 +13,39 @@ const unsigned char level[][16] PROGMEM = {
 	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
 	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
 	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
+	{ 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
 	{ SpriteData::STATUE_TOP, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
 	{ SpriteData::STATUE_BOTTOM, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, },
 	{ SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, SpriteData::BLOCK_16_8_LEFT, SpriteData::BLOCK_16_8_RIGHT, },
 };
 
+namespace MapManager
+{
+	// the vertical shift value in pixel that should be aplied to the camera when drawing the level
+	const int CAMERA_VERTICAL_SHIFT = 2;
+	const int NB_HORIZONTAL_SPRITE_PER_SCREEN = 16;
+	const int NB_VERTICAL_SPRITE_PER_SCREEN = 9;
+	
+	// the sprite width and height in pixel
+	const int LEVEL_SPRITE_WIDTH = 8;
+	const int LEVEL_SPRITE_HEIGHT = 8;
+	
+	// The camera coordinate reference the top left corner of the screen portion of the level, in the big level array.
+	int CameraX = 0;
+	int CameraY = 0;
+}
+
 void MapManager::Update()
 {
-	for (int y = 0; y < 8; ++y)
-		for (int x = 0; x < 16; ++x)
+	for (int y = 0; y < NB_VERTICAL_SPRITE_PER_SCREEN; ++y)
+		for (int x = 0; x < NB_HORIZONTAL_SPRITE_PER_SCREEN; ++x)
 		{
-			unsigned char spriteId = pgm_read_byte(&(level[y][x]));
+			unsigned char spriteId = pgm_read_byte(&(level[y + CameraY][x + CameraX]));
 			if (spriteId != SpriteData::NOTHING)
-				arduboy.drawBitmap(8*x, 8*y, SpriteData::Walls[spriteId], 8, 8, WHITE);
+				arduboy.drawBitmap(LEVEL_SPRITE_WIDTH * x,
+					(LEVEL_SPRITE_HEIGHT * y) - CAMERA_VERTICAL_SHIFT,
+					SpriteData::Walls[spriteId],
+					LEVEL_SPRITE_WIDTH, LEVEL_SPRITE_HEIGHT, WHITE);
 		}
 	
 }
