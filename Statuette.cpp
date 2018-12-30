acbb16fb096b2fb160660cc550462679276b5c26
Alban Nanty <banban@banban.org>
2018-12-30 22:14:23 +0800

    | improve the shining star of the statuette

--- Statuette.cpp
+++ Statuette.cpp
@@ -15,11 +15,31 @@ void Statuette::Update()
 	if (AnimFrameId == -1)
 	{
 		// check if we need to start a shining star animation
-		if (arduboy.everyXFrames(PAUSE_TIME))
+		if (arduboy.everyXFrames(PauseTime))
 		{
+			// init the shining star animation id
 			AnimFrameId = 0;
-			ShineStarShiftX = random(-2,3);
-			ShineStarShiftY = random(-1,3);
+			
+			// draw a random position for the shinning star
+			// first choose the vertical position.
+			ShineStarShiftY = random(-2,4);
+			// If the y position is on top, choose a free x position,
+			// otherwise we choose a x position on one of the two side of the statuette
+			if (ShineStarShiftY == -2)
+			{
+				ShineStarShiftX = random(-2,3);
+			}
+			else
+			{
+				ShineStarShiftX = random(0,2);
+				if (ShineStarShiftX == 0)
+					ShineStarShiftX = -3;
+				else
+					ShineStarShiftX = 3;
+			}
+			
+			// choose a random next pause time
+			PauseTime = random(MIN_PAUSE_TIMES, MAX_PAUSE_TIMES);
 		}
 	}
 	else
@@ -32,6 +52,7 @@ void Statuette::Update()
 		}
 	}
 	
+	// draw the statuette and the shinning star if any
 	arduboy.drawBitmap(X, Y, SpriteData::Statue, SpriteData::STATUE_SPRITE_WIDTH, SpriteData::STATUE_SPRITE_HEIGHT, WHITE);
 	if (AnimFrameId > -1)
 		arduboy.drawBitmap(X + ShineStarShiftX, Y + ShineStarShiftY, SpriteData::ShineStar[AnimFrameId], SpriteData::SHINE_STAR_SPRITE_WIDTH, SpriteData::SHINE_STAR_SPRITE_HEIGHT, INVERT);
--- Statuette.h
+++ Statuette.h
@@ -10,10 +10,17 @@ public:
 	virtual void Update();
 
 private:
-	const int ANIM_SPEED = 3;
-	const int PAUSE_TIME = 60;
+	const int ANIM_SPEED = 2;
+	const int MIN_PAUSE_TIMES = 10;
+	const int MAX_PAUSE_TIMES = 150;
+	
+	// the current time to wait before playing the next shining star
+	char PauseTime = 0;
+	
+	// current frame id and direction of the shinning star
 	char AnimFrameId = -1;
 	char AnimDirection = 1;
+	
 	// the position shift for the shine star, relative to the statuette
 	char ShineStarShiftX = 0;
 	char ShineStarShiftY = 0;
