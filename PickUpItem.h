13835b0ddf63d628bafef55e0ffe68b40c0ded84
Alban Nanty <banban@banban.org>
2019-01-05 21:23:03 +0800

    | move the code of the shiny star to the Pickup Item class + add a shiny star to the Bullet and Dynamite Crate

--- PickUpItem.h
+++ PickUpItem.h
@@ -8,6 +8,25 @@ class PickUpItem : public Item
 public:
 	PickUpItem(int startX, int startY, unsigned char flag) : Item(startX, startY, flag) {}
 	virtual void PickUp() = 0;
+
+protected:
+	void UpdateShineStar(char minX, char maxX, char minY, char maxY);
+	
+private:
+	const int ANIM_SPEED = 2;
+	const int MIN_PAUSE_TIMES = 10;
+	const int MAX_PAUSE_TIMES = 150;
+	
+	// the current time to wait before playing the next shining star
+	char PauseTime = 0;
+	
+	// current frame id and direction of the shinning star
+	char AnimFrameId = -1;
+	char AnimDirection = 1;
+	
+	// the position shift for the shine star, relative to the statuette
+	char ShineStarShiftX = 0;
+	char ShineStarShiftY = 0;
 };
 
 #endif
