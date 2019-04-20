a2bd386095a1b3c4acf96663a677a3b3cf5a31c8
Alban (Banban) Nanty <Alban (Banban) Nanty@localhost>
2019-04-20 20:09:23 +0800

    + add a walk animation for the Mummy

--- Mummy.h
+++ Mummy.h
@@ -8,6 +8,10 @@ class Mummy : public Item
 public:
 	Mummy(int startX, int startY);
 	virtual bool Update(UpdateStep step);
+	
+private:
+	unsigned char AnimFrameId : 3;
+	unsigned char AnimFrameCount : 5;
 };
 
 #endif
