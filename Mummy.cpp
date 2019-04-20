4b8d9f91be1f82c26430396ba79328ec574d94ea
Alban Nanty <banban@banban.org>
2019-04-20 20:09:23 +0800

    + add a walk animation for the Mummy

--- Mummy.cpp
+++ Mummy.cpp
@@ -8,8 +8,12 @@
 #include "Rick.h"
 #include "MapManager.h"
 
+const char WALK_ANIM_SPEED[] = { 8, 12, 4, 6 };
+
 Mummy::Mummy(int startX, int startY) : Item(startX, startY, Item::PropertyFlags::ENEMIES | Item::PropertyFlags::STATIC_COLLISION_NEEDED)
 {
+	AnimFrameId = 0;
+	AnimFrameCount = 0;
 };
 
 bool Mummy::Update(UpdateStep step)
@@ -18,10 +22,21 @@ bool Mummy::Update(UpdateStep step)
 	{
 		case Item::UpdateStep::DRAW_ENEMIES:
 		{
+			// update the animation
+			AnimFrameCount++;
+			if (AnimFrameCount == WALK_ANIM_SPEED[AnimFrameId])
+			{
+				X++;
+				AnimFrameId++;
+				AnimFrameCount = 0;
+				if (AnimFrameId == 4)
+					AnimFrameId = 0;
+			}
+			
 			// draw the statuette and check the collision with eventual lethal stuff
 			int collision = arduboy.drawBitmapExtended(MapManager::GetXOnScreen(X),
 									MapManager::GetYOnScreen(Y),
-									SpriteData::Mummy[0],
+									SpriteData::Mummy[AnimFrameId],
 									SpriteData::MUMMY_SPRITE_WIDTH, SpriteData::MUMMY_SPRITE_HEIGHT,
 									WHITE, false);
 			if (collision != 0)
@@ -34,7 +49,7 @@ bool Mummy::Update(UpdateStep step)
 		}
 		case Item::UpdateStep::CHECK_STATIC_COLLISION:
 		{
-			int yUnderFeet = Y + 12;
+			int yUnderFeet = Y + 13;
 			if (!MapManager::IsThereAnyHorizontalCollisionAt(X+1, X+7, yUnderFeet))
 				Y++;
 			break;
