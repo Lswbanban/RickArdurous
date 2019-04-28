9ed03f95b6f2a6ad7b6ee495a841e7dc3399f463
Alban (Banban) Nanty <Alban (Banban) Nanty@localhost>
2019-04-28 20:12:22 +0800

    | properly fix the bug of the skeleton falling into ground collision(when falling from a high altitude)

--- MapData.cpp
+++ MapData.cpp
@@ -38,7 +38,7 @@ Spike	sp3(100,90, Item::PropertyFlags::NONE);
 Statuette	st(40,25);
 DynamiteCrate	dc(140, 50);
 BulletCrate		bc(200, 40);
-Enemy		mum(84, 15, Item::PropertyFlags::SPECIAL | Item::PropertyFlags::MIRROR_X);
+Enemy		mum(70, 1, Item::PropertyFlags::SPECIAL | Item::PropertyFlags::MIRROR_X);
 
 // The array that contains all the items
 Item * MapManager::Items[] = {
