f6d80df4e4ca248bd95449888c09d2d15bef38f5
Alban Nanty <banban@banban.org>
2019-05-05 17:48:35 +0800

    | implement the logic of the stalagmite (destruction and the sparks effect)

--- Stalagmite.h
+++ Stalagmite.h
@@ -8,6 +8,12 @@ class Stalagmite : public DestroyableItem
 public:
 	Stalagmite(int startX, int startY, unsigned char flags);
 	virtual bool Update(UpdateStep step);
+private:
+	static constexpr int SPARKS_ANIM_SPEED = 3;
+
+	unsigned char SparksAnimFrameId = 0;
+	
+	void Draw();
 };
 
 #endif
