39bf0bbfd952fc6f9ca0de22b9d4248387721a68
Alban (Banban) Nanty <Alban (Banban) Nanty@localhost>
2019-05-05 16:59:24 +0800

    | implement the breaking state of the stalactite

--- Stalactite.h
+++ Stalactite.h
@@ -10,17 +10,22 @@ public:
 	virtual bool Update(UpdateStep step);
 
 protected:
+	static constexpr int SHAKING_SPEED = 120;
+	static constexpr int SPARKS_ANIM_SPEED = 3;
+
 	enum State
 	{
 		WAIT = 0,
 		FALL,
-		DEAD,
+		BREAKING,
 	};
 	
 	unsigned char AnimState = 0;
+	unsigned char SparksAnimFrameId = 0;
 	
 	static void CheckTrigererCallback(Item * me, Item * trigerer);
 	void CheckTrigerer(bool isAlive, int trigererX, int trigererY);
+	void Draw(int shakeX);
 };
 
 #endif
