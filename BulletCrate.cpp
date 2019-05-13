ae1530cc03482c3d4d3cc367bdd2a8f036a84434
Alban (Banban) Nanty <Alban (Banban) Nanty@localhost>
2019-05-13 21:28:42 +0800

    | fix the respawn logic of the BulletCrate and the DynamiteCrate

--- BulletCrate.cpp
+++ BulletCrate.cpp
@@ -24,6 +24,7 @@ bool BulletCrate::Update(UpdateStep step)
 				{
 					ClearProperty(Item::PropertyFlags::ALIVE);
 					Rick::BulletCount = Rick::MAX_BULLET_COUNT;
+					return true;
 				}
 				
 				// draw the crate
@@ -32,6 +33,10 @@ bool BulletCrate::Update(UpdateStep step)
 				// draw the shiny star
 				UpdateShineStar(-2, 9, 1, 6);
 			}
+			break;
+			
+		case UpdateStep::RESPAWN:
+			SetProperty(Item::PropertyFlags::ALIVE);
 			break;
 	}
 	return false;
