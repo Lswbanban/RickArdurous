e2d0d4db48ed9bc1ce42437164642d95979ec223
Alban (Banban) Nanty <Alban (Banban) Nanty@localhost>
2019-05-14 21:59:43 +0800

    | bug fix: fix the DestroyableItem that were destroyed when outside of the screen

--- DestroyableItem.cpp
+++ DestroyableItem.cpp
@@ -12,21 +12,27 @@ DestroyableItem::DestroyableItem(unsigned char flags) : Item(flags)
 
 bool DestroyableItem::CheckLethalDynamite(int width)
 {
-	// get my coordinate on screen
-	int xOnScreen = MapManager::GetXOnScreen(X);
+	// get my y coordinate on screen
 	int yOnScreen = MapManager::GetYOnScreen(Y);
-	
-	// check on the left
-	for (int i = 1; i <= EXPLOSION_DETECTION_DISTANCE; ++i)
-		if (arduboy.getPixel(xOnScreen - i, yOnScreen) == WHITE)
-			return true;
-	
-	// check on the right
-	xOnScreen += width;
-	for (int i = 1; i <= EXPLOSION_DETECTION_DISTANCE; ++i)
-		if (arduboy.getPixel(xOnScreen + i, yOnScreen) == WHITE)
-			return true;
-	
+	// check if I'm on screen
+	if ((yOnScreen >= 0) && (yOnScreen < HEIGHT))
+	{
+		// get my x coordinate on screen
+		int xOnScreen = MapManager::GetXOnScreen(X);
+		
+		// iterate on a certain distance
+		for (int i = 1; i <= EXPLOSION_DETECTION_DISTANCE; ++i)
+		{
+			// check on the left
+			int xLeft = xOnScreen - i;
+			if ((xLeft >= 0) && (xLeft < WIDTH) && (arduboy.getPixel(xLeft, yOnScreen) == WHITE))
+				return true;
+			// check on the right
+			int xRight = xOnScreen + width + i;
+			if ((xRight >= 0) && (xRight < WIDTH) && (arduboy.getPixel(xRight, yOnScreen) == WHITE))
+				return true;
+		}
+	}
 	// by default no collision detected
 	return false;
 }
