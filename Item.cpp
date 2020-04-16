12aae339ca28a4c57089ca40da0c59b2bdff767e
Alban Nanty <banban@banban.org>
2020-04-16 21:06:13 +0800

    | Move the Init function that ask the Progress from the Enemy to the Item class (for using it for all items) | fix the bug related to the scorpion, created when we move the constructor to the Init function

--- Item.cpp
+++ Item.cpp
@@ -4,6 +4,7 @@
 
 #include "RickArdurous.h"
 #include "Item.h"
+#include "Progress.h"
 
 void Item::Init(int startX, int startY, unsigned char flags)
 {
@@ -13,3 +14,15 @@ void Item::Init(int startX, int startY, unsigned char flags)
 	// call the update virtual function with the Respawn step, for specific data init
 	Update(Item::UpdateStep::RESPAWN);
 }
+
+void Item::Init(int startX, int startY, unsigned char flags, bool shouldRespawn)
+{
+	// if we are not forced to respaw, ask our alive status to the progress
+	if (!shouldRespawn)
+		shouldRespawn = Progress::IsItemAlive(this);
+	// set the alive flag
+	if (shouldRespawn)
+		flags |= Item::PropertyFlags::ALIVE;
+	// and call the other init function
+	Init(startX, startY, flags);
+};
