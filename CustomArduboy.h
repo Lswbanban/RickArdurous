e8545d282dc8ca7df1d2b46f37fefd57731e875a
Alban Nanty <banban@banban.org>
2020-04-26 23:10:44 +0800

    | finalize the game over screen

--- CustomArduboy.h
+++ CustomArduboy.h
@@ -10,6 +10,8 @@ class CustomArduboy : public Arduboy
 public:
 	unsigned int drawBitmapExtended(int16_t x, int16_t y, const uint8_t *bitmap, uint8_t w, uint8_t h, uint8_t color, bool mirrorX);
 	bool CheckWhitePixelsInRow(uint8_t x, uint8_t y, uint8_t w);
+	uint8_t GetCursorX() { return cursor_x; }
+	uint8_t GetCursorY() { return cursor_y; }
 };
 
 #endif
