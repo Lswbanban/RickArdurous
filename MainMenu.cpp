997753c294c6abf30089816bce17cf3cdba13e54
Alban (Banban) Nanty <Alban (Banban) Nanty@localhost>
2020-04-26 23:10:44 +0800

    | finalize the game over screen

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -9,17 +9,26 @@
 #include "MapData.h"
 #include "SpriteData.h"
 #include "Graal.h"
+#include "Rick.h"
 
 namespace MainMenu
 {
 	static constexpr int LAST_MENU_OPTION = 1;
 	static constexpr int MENU_X = 20;
+	static constexpr int GAME_OVER_ANIM_SPEED = 4;
 	
 	unsigned char SelectedOption = 0;
 	
+	// variable for game over animation
+	char SpriteY;
+	unsigned char GameOverAnimId = SpriteData::RickAnimFrameId::DEATH_START;
+	char GameOverAnimDirection = -1;
+	
 	void DrawMainMenu();
+	void DrawStatuetteCount();
 	void DrawVictory();
 	void DrawGameOver();
+	void InitGameOver();
 }
 
 void MainMenu::UpdateMainMenu()
@@ -34,6 +43,7 @@ void MainMenu::UpdateMainMenu()
 		switch (SelectedOption)
 		{
 			case 0:
+				InitGameOver();
 				GameManager::StartNewGame();
 				break;
 			case 1:
@@ -71,21 +81,60 @@ void MainMenu::DrawMainMenu()
 	arduboy.print('>');
 }
 
+void MainMenu::DrawStatuetteCount()
+{
+	arduboy.print(Rick::StatuetteCount, DEC);
+	arduboy.drawBitmapExtended(arduboy.GetCursorX() + 2, arduboy.GetCursorY() -1, SpriteData::Statuette, SpriteData::STATUETTE_SPRITE_WIDTH, SpriteData::STATUETTE_SPRITE_HEIGHT, WHITE, false);
+	arduboy.setCursor(arduboy.GetCursorX() + (8 + SpriteData::STATUETTE_SPRITE_WIDTH), arduboy.GetCursorY());
+}
+
 void MainMenu::DrawVictory()
 {
 	// draw the graal!
-	arduboy.drawBitmapExtended(61, 10, SpriteData::Graal, SpriteData::GRAAL_SPRITE_WIDTH, SpriteData::GRAAL_SPRITE_HEIGHT, WHITE, false);
-	graal1.UpdateShineStar(61, 10, -2, 3, -2, 4);
+	arduboy.drawBitmapExtended(61, 8, SpriteData::Graal, SpriteData::GRAAL_SPRITE_WIDTH, SpriteData::GRAAL_SPRITE_HEIGHT, WHITE, false);
+	graal1.UpdateShineStar(61, 8, -2, 3, -2, 4);
 
 	// draw the message
-	arduboy.setCursor(19, 30);
+	arduboy.setCursor(21, 26);
 	arduboy.print("Congratulation!");
-	arduboy.setCursor(4, 44);
-	arduboy.print("You found the Graal.");
+	arduboy.setCursor(7, 46);
+	arduboy.print("You found the Graal");
+	arduboy.setCursor(43, 56);
+	arduboy.print("and ");
+	DrawStatuetteCount();
+}
+
+void MainMenu::InitGameOver()
+{
+	SpriteY = 63;
+	GameOverAnimId = SpriteData::RickAnimFrameId::DEATH_START;
+	GameOverAnimDirection = -1;
 }
 
 void MainMenu::DrawGameOver()
 {
-	arduboy.setCursor(20, 40);
+	// updat the animation of Rick sprite
+	if (arduboy.everyXFrames(GAME_OVER_ANIM_SPEED))
+	{
+		// reverse direction if we reach the end of the loop
+		if ((GameOverAnimId == SpriteData::RickAnimFrameId::DEATH_START) ||
+			(GameOverAnimId == SpriteData::RickAnimFrameId::DEATH_END))
+			GameOverAnimDirection = -GameOverAnimDirection;
+		// increase the anim frame
+		GameOverAnimId += GameOverAnimDirection;
+		// decrease altitude
+		SpriteY--;
+	}
+	// draw rick sprite
+	arduboy.drawBitmapExtended(GameOverAnimId - SpriteData::RickAnimFrameId::DEATH_START, SpriteY, SpriteData::Rick[GameOverAnimId], SpriteData::RICK_SPRITE_WIDTH, SpriteData::RICK_SPRITE_HEIGHT, WHITE, false);
+
+	arduboy.setCursor(16, 8);
 	arduboy.print("Game Over...");
+	arduboy.setCursor(16, 26);
+	arduboy.print("You died before");
+	arduboy.setCursor(16, 36);
+	arduboy.print("reaching the Graal");
+	arduboy.setCursor(16, 54);
+	DrawStatuetteCount();
+	arduboy.print("collected");
 }
